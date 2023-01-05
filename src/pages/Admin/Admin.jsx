eb06828812bfc20d612adfc27752c26698c31135
L K Livingstone <livingstone.lk.337@gmail.com>
2023-01-06 00:45:11 +0530

    redirect

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import "./Admin.css"
 import { logOut } from "../../redux/userRedux";
-
+import { useState } from "react";
 
 const Admin = () => {
 
@@ -18,6 +18,30 @@ const Admin = () => {
 
         navigate("/login")
 	}  
+
+    const [name, setName]= useState("")
+    const [degree, setDegree]= useState("")
+    const [university, setUniversity]= useState("")
+    const [website, setWebsite]= useState("")
+    const [location, setLocation]= useState([])
+    const [fees, setFees]= useState()
+    const [duration, setDuration]= useState("")
+    const [fulltime, setFulltime]= useState(true)
+    const [campus, setCampus]= useState("")
+    const [about, setAbout]= useState("")
+    const [rating, setRating]= useState()
+    const [ranking, setRanking]= useState()
+    const [credits, setCredits]= useState("")
+    const [language, setLanguage]= useState("")
+    const [start, setStart]= useState("")
+    const [apply, setApply]= useState("")
+    const [disciplines, setDisciplines]= useState([])
+    const [overview, setOverview]= useState("")
+    const [programmestructure, setProgrammestructure]= useState([])
+    const [acadrequirements, setAcadrequirements]= useState(" ")
+    const [engrequirements, setEngrequirements]= useState([])
+    const [otherrequirements, setOtherrequirements]= useState([])
+    const [cost, setCost]= useState()
 
     return (
         <>
@@ -40,8 +64,56 @@ const Admin = () => {
                     </ul>
                 </div>
             </nav>
-            <div>
-                
+            <div style={{padding: "20vh"}}>
+                <div>
+                    <input placeholder="Name of the study" onChange={(e)=>setName(e.target.value)} />
+                    <button type="submit" >Submit</button>
+                </div>
+                <div>
+                    <input placeholder="email" onChange={(e)=>setDegree(e.target.value)} />
+                    <button type="submit" >Submit</button>
+                </div>
+                <div>
+                    <input placeholder="email" onChange={(e)=>setUniversity(e.target.value)} />
+                    <button type="submit" >Submit</button>
+                </div>
+                <div>
+                    <input placeholder="Name of the study" onChange={(e)=>setWebsite(e.target.value)} />
+                    <button type="submit" >Submit</button>
+                </div>
+                <div>
+                    <input placeholder="email" onChange={(e)=>setDegree(e.target.value)} />
+                    <button type="submit" >Submit</button>
+                </div>
+                <div>
+                    <input placeholder="email" onChange={(e)=>setFees(e.target.value)} />
+                    <button type="submit" >Submit</button>
+                </div>
+
+                <div>
+                    <input placeholder="Name of the study" onChange={(e)=>setDuration(e.target.value)} />
+                    <button type="submit" >Submit</button>
+                </div>
+                <div>
+                    <input type="checkbox" name="favorite1" value="chocolate" /> Chocolate
+                    <input type="checkbox" name="favorite2" value="vanilla" /> Vanilla
+                </div>
+                <div>
+                    <input placeholder="On Campus or Off Campus" onChange={(e)=>setCampus(e.target.value)} />
+                    <button type="submit" >Submit</button>
+                </div>
+                <div>
+                    <input placeholder="Name of the study" onChange={(e)=>setWebsite(e.target.value)} />
+                    <button type="submit" >Submit</button>
+                </div>
+                <div>
+                    <input placeholder="email" onChange={(e)=>setDegree(e.target.value)} />
+                    <button type="submit" >Submit</button>
+                </div>
+                <div>
+                    <input placeholder="email" onChange={(e)=>setFees(e.target.value)} />
+                    <button type="submit" >Submit</button>
+                </div>
             </div>
         </>
     )
